ed3c3daef979dad1f942c4cee79c79f6f8273137
Bhagabati Prasad <bhagabatiprasada@gmail.com>
2020-06-28 21:13:57 +0530

    initial commit

---
+++ style.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$(".toggle").click(function(){
+		$(".togglediv").fadeIn();
+		$(".menu").fadeIn(1400);
+	});
+	$(".close").click(function(){
+		$(".togglediv").fadeOut();
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
